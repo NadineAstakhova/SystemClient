0cf418d45b96b4c7c3dd65f3c3dce4dc31aede31
Nadine <nadine.astakhova@gmail.com>
2017-01-21 18:53:49 +0200

    add comments

--- Server/source/app.js
+++ Server/source/app.js
@@ -15,7 +15,7 @@ app.use(bodyparser.json());
 
 connection.init();
 routes.configure(app);
- 
+//standard function for server  
 server.listen(8000, function() {
   console.log('Server listening on port ' + server.address().port);
 });
@@ -29,39 +29,33 @@ app.post('/',function(req,res){
     res.send('ok');
 });
 
-
-
+//next  functions describe how server send info for push message
 io.on('connection', function(socket){
 	console.log("User Connected!");
-    
-    var idMess;
-    var idUs;
-        
+	var idMess;
+	var idUs;
+        //server monitors the connection of a new user
 	socket.emit('socketID', { id: socket.id });
     socket.on('getNewUser', function (message){
+	//push new user to users with unique id
         users.push(new user(socket.id, message));
         idUs = message;
+	//check function is repeated every 10 seconds
         maybe(idUs, wait10sec);
         console.log('New user with id: ' + message);
-       
     });
+    //this function monitors disable user and delete him from users
     socket.on('disconnectUser', function(message){
-        //console.log("User Disconnected "+ message);
-        
-		for(var i = 0; i < users.length; i++){
-			if(users[i].idUser === message){
-                console.log("User Disconnected " + users[i].idUser);
-				users.splice(i, 1);
-                idUs = null;
-			}
-		}
+	    for(var i = 0; i < users.length; i++){
+		    if(users[i].idUser === message){
+			    console.log("User Disconnected " + users[i].idUser);
+			    users.splice(i, 1);
+			    idUs = null;
+		    }
+	    }
 	}); 
-    socket.on('idUser', function (message) {
-      // console.log("ну а что");
-           
-        
-    }); 
-    
+    socket.on('idUser', function (message) {}); 
+   //callback function send request to database aboit new works for users 
    function maybe(idUs,  callback){
         console.log('A client is speaking to me! They’re saying: ' + idUs);
         for(var i = 0; i < users.length; i++){
@@ -73,6 +67,7 @@ io.on('connection', function(socket){
         }
         callback();
    }
+   //this function send new message to user if it exists
    function t(rows) {
        if(rows.length  !== 0) {
            var res = ""+rows[rows.length-1].new_task+" for "+rows[rows.length-1].subject+" by "+rows[rows.length-1].author;
@@ -84,18 +79,18 @@ io.on('connection', function(socket){
                message:  res,
                idDel: idDelMess
            });
+	   //delete new message from database when it was sent
            todo.deleteOldMes(rows[rows.length-1].id_work);    
         }
         else
           console.log("no message");  
-        }
-	function wait10sec(){
+   }
+ //repeat function with timeout
+  function wait10sec(){
         setTimeout(function(){
-            //todo.getNewMess(idUs, wait10sec).then(t).catch((err) => setImmediate(() => { throw err; }));
             maybe(idUs, wait10sec);
         }, 10000);
-    }
-    //io.sockets.socket(socketid).emit('eventClient', 'for your eyes only');
+  }
 });
 
 function user(idSocket, idUser){
